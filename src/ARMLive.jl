6d6d9e60d998853255b7b09f37aa0ccb198e6358
Nathanael Wong <natgeo.wong@outlook.com>
2025-10-11 20:31:18 -0400

    Setting up necessary dependencies

--- src/ARMLive.jl
+++ src/ARMLive.jl
@@ -1,5 +1,19 @@
 module ARMLive
 
-# Write your package code here.
+## Base Modules Used
+using DelimitedFiles
+using Logging
+using Printf
+using Statistics
+
+import Base: show, read, download
+
+## Modules Used
+using PrettyTables
+
+## Reexporting exported functions within these modules
+using Reexport
+@reexport using Dates
+@reexport using NCDatasets
 
 end
